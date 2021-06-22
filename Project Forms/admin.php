c5cdb8317b60d568bcd4093155dc3802bea151f0
Maheen <mushrifmaheen35@gmail.com>
2021-06-22 13:48:08 +0600

    Project form

---
+++ Project Forms/admin.php
@@ -0,0 +1,87 @@
+<html>
+	<head>
+		<title>
+			Admin
+		</title>
+	</head>
+	
+	<body align = "center">
+		<h1>
+			Admin
+		</h1>
+		
+		<form>
+			<table align = "center">
+				<tr>
+					<td>
+						Admin Name:
+					</td>
+					
+					<td>
+						<input type= "text" placeholder= "Name" name= "name"
+					</td>
+				</tr>
+				
+				<tr>
+					<td>
+						Admin ID:
+					</td>
+					
+					<td>
+						<input type= "text" placeholder= "ID" name= "id"
+					</td>
+				</tr>
+				
+				<tr>
+					<td>
+						Admin Password:
+					</td>
+					
+					<td>
+						<input type= "text" placeholder= "Password" name= "password"
+					</td>
+				</tr>
+				
+				<tr>
+					<td>
+						Admin Phone:
+					</td>
+					
+					<td>
+						<input type= "text" placeholder= "Phone number" name= "phone"
+					</td>
+				</tr>
+				
+				<tr>
+					<td>
+						Admin Email:
+					</td>
+					
+					<td>
+						<input type= "text" placeholder= "Email" name= "email"
+					</td>
+				</tr>
+				
+				<tr>
+					<td>
+						Admin Address:
+					</td>
+					
+					<td>
+						<input type= "text" placeholder= "Address" name= "addres"
+					</td>
+				</tr>
+				
+				<tr>
+					<td>
+						
+					</td>
+					
+					<td>
+						<input type= "submit" value="Log in">
+					</td>
+				</tr>
+			</table>
+		</form>
+	</body>
+</html>
---
+++ Project Forms/employee.php
@@ -0,0 +1,87 @@
+<html>
+	<head>
+		<title>
+			Employee
+		</title>
+	</head>
+	
+	<body align = "center">
+		<h1>
+			Employee
+		</h1>
+		
+		<form>
+			<table align = "center">
+				<tr>
+					<td>
+						Employee Name:
+					</td>
+					
+					<td>
+						<input type= "text" placeholder= "Name" name= "name"
+					</td>
+				</tr>
+				
+				<tr>
+					<td>
+						Employee ID:
+					</td>
+					
+					<td>
+						<input type= "text" placeholder= "ID" name= "id"
+					</td>
+				</tr>
+				
+				<tr>
+					<td>
+						Employee Password:
+					</td>
+					
+					<td>
+						<input type= "text" placeholder= "Password" name= "password"
+					</td>
+				</tr>
+				
+				<tr>
+					<td>
+						Employee Phone:
+					</td>
+					
+					<td>
+						<input type= "text" placeholder= "Phone number" name= "phone"
+					</td>
+				</tr>
+				
+				<tr>
+					<td>
+						Employee Email:
+					</td>
+					
+					<td>
+						<input type= "text" placeholder= "Email" name= "email"
+					</td>
+				</tr>
+				
+				<tr>
+					<td>
+						Employee Address:
+					</td>
+					
+					<td>
+						<input type= "text" placeholder= "Address" name= "addres"
+					</td>
+				</tr>
+				
+				<tr>
+					<td>
+						
+					</td>
+					
+					<td>
+						<input type= "submit" value="Log in">
+					</td>
+				</tr>
+			</table>
+		</form>
+	</body>
+</html>
